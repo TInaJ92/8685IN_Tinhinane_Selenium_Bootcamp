f2f9422651e71c967e0367fb7b5c674ec596997a
TInaJ92 <tinatebany@gmail.com>
2023-07-08 20:49:44 -0400

    initial commit

--- com.optimum/src/main/java/homepage/HomePage.java
+++ com.optimum/src/main/java/homepage/HomePage.java
@@ -1,4 +1,16 @@
 package homepage;
 
-public class HomePage {
+import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+    public HomePage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//button[@class=\"header-account-button\"]")
+    public WebElement signInButton;
+
 }
